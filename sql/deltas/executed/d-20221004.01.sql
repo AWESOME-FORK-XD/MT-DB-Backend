9365d1511d03cc13efa6d88b42a03284b5217d88
gaumeister <derek@apigrate.com>
2022-10-05 13:22:34 -0500

    removed table from production

--- sql/deltas/executed/d-20221004.01.sql
+++ sql/deltas/executed/d-20221004.01.sql
@@ -1,6 +1,6 @@
 /* 
   Removes the product family (t_product_family) table from the database.
   Created: 10/04/2022 by Derek Gau
-  Deployed:
+  Deployed: 10/05/2022 by Derek Gau
 */
 drop table t_product_family;
