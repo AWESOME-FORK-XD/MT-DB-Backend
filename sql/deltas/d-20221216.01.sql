ff285d54516b9c864880a936280144a1bb12ef71
gaumeister <derek@apigrate.com>
2023-01-03 14:55:17 -0600

    drop opc visible

--- sql/deltas/d-20221216.01.sql
+++ sql/deltas/d-20221216.01.sql
@@ -1,8 +1,13 @@
 /* 
   Adds `is_popular_branded` column to t_brand table.
+
+  Remove deprecated `opc_visible` field from t_filter table.
+  
   Created: 12/16/2022 by Derek Gau
   Deployed to TEST: 12/16/2022 by Derek Gau
   Deployed to PROD: 
 
 */
 alter table t_brand add column `is_popular_branded` bit not null;
+
+alter table t_filter drop column `opc_visible`;
