e71d12c7a4fac96c5747ae8273192afc83ae261b
gaumeister <derek@apigrate.com>
2023-01-03 15:18:36 -0600

    Document DDL process.

---
+++ README-DB-CHANGES.md
@@ -0,0 +1,8 @@
+# Managing Database Structural Changes
+
+## Adding Fields to a Database Table/View
+
+
+## Appendix
+
+[App1 API](https://api.app1example.com)
---
+++ sql/deltas/d-YYYYMMDD.nn.sql
@@ -0,0 +1,13 @@
+/* 
+  Summarize database change.
+
+  Provide a short list of database changes in this script.
+  
+  Created:          MM/DD/YYYY by (author)
+  Deployed to TEST: MM/DD/YYYY by (author)
+  Deployed to PROD: MM/DD/YYYY by (author)
+
+  Note: please keep any data load scripts in a separate file. Structural changes should generally not be mixed with data additions.
+*/
+
+DDL Statements here...
