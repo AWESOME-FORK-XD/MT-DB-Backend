c0099af20e325613fe0760c264db02cf3f2cfa03
gaumeister <derek@apigrate.com>
2023-01-04 09:36:55 -0600

    column additions for cost, etc

--- sql/deltas/executed/d-20221216.01.sql
+++ sql/deltas/executed/d-20221216.01.sql
@@ -5,7 +5,7 @@
   
   Created: 12/16/2022 by Derek Gau
   Deployed to TEST: 12/16/2022 by Derek Gau
-  Deployed to PROD: 
+  Deployed to PROD: 1/4/2023 by Derek Gau
 
 */
 alter table t_brand add column `is_popular_branded` bit not null;
